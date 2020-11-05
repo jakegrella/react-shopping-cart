b6bb9f2ee7971fbb91c709096892e5dec532bf2b
Jake Grella <jgrella13@gmail.com>
2020-11-05 14:03:16 -0600

    completed mvp

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // Components
 import Product from './Product';
 
-const Products = props => {
+//context
+import { ProductContext } from '../contexts/ProductContext';
+
+const Products = () => {
+	const { products, addItem } = useContext(ProductContext);
+
 	return (
-		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
+		<div className='products-container'>
+			{products.map((product) => (
+				<Product key={product.id} product={product} addItem={addItem} />
 			))}
 		</div>
 	);
